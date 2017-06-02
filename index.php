cdbc8a5470eed759ba7fbcabb0803d80f7b0d6a9
My Next Ford Pairing Station <mvf@Marks-iMac.fmcc.fc.ford.com>
2017-06-02 15:22:56 -0400

    change working on static page

--- index.php
+++ index.php
@@ -1,7 +1,2 @@
-<html>
-  <head>
-    <title>Sample "Hello, World" Application</title>
-  </head>
-  <body bgcolor=yellow>
-          <h1>Sample "Hello, World" Application</h1>
-  </body>
+<?php include_once("home.html"); ?>
+
